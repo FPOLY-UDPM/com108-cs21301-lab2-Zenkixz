57f27f9ef6269693cb44f396863cc45982e09faa
Zenkixz <senjixz@gmail.com>
2026-01-13 08:53:17 +0700

    com108-cs21301-lab2-AnhQuan

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [NGUYỄN HUỲNH ANH QUÂN]
+ * MSSV:      [PS48285]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
  // BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH CỘNG / TRỪ 2 SỐ 
@@ -12,12 +12,18 @@
 
 int main(){
     // Khai báo biến
+    int a, b, sum, diff;
 
     // Nhập dữ liệu
-
+    printf("Nhap hai so a va b: ");
+    scanf("%d %d", &a, &b);
 
     // Xử lý, tính toán
+    sum = a + b;
+    diff = a - b;
 
     // Hiển thị kết quả
-
+    printf("Tong cua hai so la: %d\n", sum);
+    printf("Hieu cua hai so la: %d\n", diff);
+    return 0;
 }
